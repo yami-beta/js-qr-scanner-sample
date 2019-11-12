65b3ea289687598bca5e0265d523da84b829f2b8
yami-beta <yami-beta@users.noreply.github.com>
2019-11-12 14:59:02 +0900

    feat: add modal

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,32 +1,17 @@
 /** @jsx jsx */
-import React, { useState, Fragment, MouseEvent } from "react";
+import React, { Fragment } from "react";
 import { Global, css, jsx } from "@emotion/core";
 import { Header } from "./Header";
 import { QRScanner } from "./QRScanner";
 
 const App: React.FC<{}> = () => {
-  const [showScanner, setShowScanner] = useState<boolean>(false);
-  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    setShowScanner(prev => !prev);
-  };
-
   return (
     <Fragment>
       <Global styles={globalStyle} />
       <div>
         <Header text="QR Scanner" />
         <div css={contentAreaStyle}>
-          <div css={buttonListStyle}>
-            <button
-              css={showScanner ? stopButtonStyle : buttonStyle}
-              onClick={handleClick}
-            >
-              {showScanner ? "カメラを停止する" : "カメラを起動する"}
-            </button>
-          </div>
-
-          {showScanner && <QRScanner />}
+          <QRScanner />
         </div>
       </div>
     </Fragment>
@@ -41,29 +26,6 @@ const globalStyle = css`
 
 const contentAreaStyle = css`
   padding: 10px;
-`;
-const buttonListStyle = css`
-  display: flex;
-  justify-content: center;
-  padding: 10px 0;
-`;
-const buttonStyle = css`
-  padding: 0.5em;
-  outline: none;
-  color: rgba(255, 255, 255, 1);
-  background-color: rgba(0, 120, 212, 1);
-  border: none;
-  cursor: pointer;
-  &:hover {
-    background-color: rgba(0, 120, 212, 0.8);
-  }
-`;
-const stopButtonStyle = css`
-  ${buttonStyle};
-  background-color: rgba(232, 17, 35, 1);
-  &:hover {
-    background-color: rgba(232, 17, 35, 0.8);
-  }
 `;
 
 export { App };
